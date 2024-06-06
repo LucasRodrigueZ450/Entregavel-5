ea7f8fe8238951a1f61f299bf4f55b72a708256d
Lucas Rodrigues <lucasrodriguessl10@gmail.com>
2024-06-06 16:55:30 -0300

    Personalize o conteúdo inicial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Bem-vindo ao Entregavel-5</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Esta é a sua aplicação React hospedada no AWS Amplify!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
